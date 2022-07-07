10b4e35ce96dd0cfaf9c11a21b2c6d546b2416be
Quentin Girard <quentin.girard@wavemind.ch>
2022-07-07 09:49:57 +0200

    Add auth check in cookies

--- store/credentials/index.js
+++ store/credentials/index.js
@@ -2,6 +2,7 @@
  * The external imports
  */
 import { createSlice } from "@reduxjs/toolkit";
+import { setCookie } from 'cookies-next';
 
 /**
  * The internal imports
@@ -14,11 +15,27 @@ const slice = createSlice({
   extraReducers: builder => {
     builder.addMatcher(
       authApi.endpoints.newSession.matchFulfilled,
-      (_state, { payload }) => payload
+      (_state, { payload }) => {
+        setCookie('sessions', {
+          accessToken: payload.accessToken,
+          client: payload.client,
+          expiry: payload.expiry,
+          uid: payload.uid,
+        }, { expires: payload.expiry })
+        return payload
+      }
     );
     builder.addMatcher(
       authApi.endpoints.authenticate.matchFulfilled,
-      (_state, { payload }) => payload
+      (_state, { payload }) => {
+        setCookie('sessions', {
+          accessToken: payload.accessToken,
+          client: payload.client,
+          expiry: payload.expiry,
+          uid: payload.uid,
+        }, { expires: payload.expiry })
+        return payload
+      }
     );
   },
 });
