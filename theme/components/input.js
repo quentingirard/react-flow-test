dd9e3312ce9a4cca3e7ee07060672eba1e840f00
Quentin Girard <quentin.girard@wavemind.ch>
2022-07-07 08:49:35 +0200

    Customize input

---
+++ theme/components/input.js
@@ -0,0 +1,26 @@
+export const inputStyles = {
+  components: {
+    Input: {
+      sizes: {
+        md: {
+          field: {
+            borderRadius: '15px',
+          },
+        },
+        lg: {
+          field: {
+            borderRadius: '15px',
+          },
+        },
+      },
+      variants: {
+        outline: {
+          field: {
+            background: "inherit",
+            border: "1px solid",
+          },
+        },
+      }
+    },
+  }
+}
